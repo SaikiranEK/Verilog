d759b7aeb53577d56e4621f53f223265bbd998ad
SaikiranEK <saikiran.e.k.1820@gmail.com>
2026-04-07 19:21:15 +0530

    Create decoder.v

---
+++ Combinational/decoder.v
@@ -0,0 +1,10 @@
+module decoder(input [1:0] i,output reg [3:0] x);
+  always @(*) begin
+    case(i)
+      2'b00: x=4'b0001;
+      2'b01: x=4'b0010;
+      2'b10: x=4'b0100;
+      2'b11: x=4'b1000;
+    endcase
+  end
+endmodule
