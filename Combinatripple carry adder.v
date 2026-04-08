3fe97f553873caa35a534e40fd4fc57f76aba801
SaikiranEK <saikiran.e.k.1820@gmail.com>
2026-04-08 22:48:04 +0530

    Create Combinatripple carry adder.v

---
+++ Combinatripple carry adder.v
@@ -0,0 +1,13 @@
+module full_adder(input a,b,c, output x,y);
+assign x=a^b^c;
+assign y=(a&b)|(b&c)|(a&c);
+endmodule
+
+module rca(input [3:0] a,b,
+	   input c,
+	   output [3:0]x,y);
+full_adder f1(a[0],b[0],c,x[0],y[0]);
+full_adder f2(a[1],b[1],y[0],x[1],y[1]);
+full_adder f3(a[2],b[2],y[1],x[2],y[2]);
+full_adder f4(a[3],b[3],y[2],x[3],y[3]);
+endmodule
