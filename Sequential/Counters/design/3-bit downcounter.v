746c91785429881d4cfc9234eb982ae73299b291
SaikiranEK <saikiran.e.k.1820@gmail.com>
2026-04-12 23:31:49 +0530

    Create 3-bit downcounter.v

---
+++ Sequential/Counters/design/3-bit downcounter.v
@@ -0,0 +1,10 @@
+module downcount_3(input clk,
+        		 input rst,
+                 output reg [2:0] count);
+  always@(posedge clk or posedge rst) begin
+    if(rst)
+      count<=0;
+    else
+      count<=count-1;
+  end
+endmodule
