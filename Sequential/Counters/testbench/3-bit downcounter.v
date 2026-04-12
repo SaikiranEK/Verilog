73bf00133c35928e6de725543896f755230346d7
SaikiranEK <saikiran.e.k.1820@gmail.com>
2026-04-12 23:32:26 +0530

    Create 3-bit downcounter.v

---
+++ Sequential/Counters/testbench/3-bit downcounter.v
@@ -0,0 +1,21 @@
+module tb;
+  reg clk;
+  reg rst;
+  wire [2:0]count;
+  downcount_3 uut(.clk(clk),.rst(rst),.count(count));
+  always #5 clk = ~clk;
+  initial begin
+    clk=0;
+    rst=1;
+    $dumpfile("dump.vcd");
+    $dumpvars(0,tb);
+    $display("clk rst | count");
+    $monitor("%b %b |  %b",clk,rst,count);
+    #10 rst = 0;
+    #100;
+    rst = 1;
+    #10 rst = 0;
+    #80 $finish;
+  end
+
+endmodule
