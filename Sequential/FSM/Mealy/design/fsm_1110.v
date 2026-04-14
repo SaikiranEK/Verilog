410fd7a37d277596fa5fc7eb3c62898166fc4475
SaikiranEK <saikiran.e.k.1820@gmail.com>
2026-04-14 22:13:03 +0530

    Create fsm_1110.v

---
+++ Sequential/FSM/Mealy/design/fsm_1110.v
@@ -0,0 +1,26 @@
+module mealy_1110(input clk,
+                  input rst,
+                  input x,
+                  output y);
+  reg [1:0] state,next;
+  parameter s0=2'b00,
+  			s1=2'b01,
+  			s2=2'b10,
+  			s3=2'b11;
+  always@(posedge clk or posedge rst) begin
+    if(rst)
+      state<=s0;
+    else
+      state<=next;
+  end
+  always@(*)begin
+    case(state)
+      s0: next=(x)?s1:s0;
+      s1: next=(x)?s2:s0;
+      s2: next=(x)?s3:s0;
+      s3: next=(x)?s1:s0;
+      default: next=s0;
+    endcase
+  end
+  assign y=(state == s3 && x==0);
+endmodule
