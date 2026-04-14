0ca197fb2d5606398a0a9c2686d2458ef3d1a49f
SaikiranEK <saikiran.e.k.1820@gmail.com>
2026-04-14 21:49:50 +0530

    Add testbench for Moore state machine

---
+++ Sequential/FSM/Moore/testbench/fesm_1110.v
@@ -0,0 +1,25 @@
+module tb;
+  reg clk;
+  reg rst;
+  reg x;
+  wire y;
+  integer i;
+  moore_1110 uut (.clk(clk),.rst(rst),.x(x),.y(y));
+  always #5 clk=~clk;
+  reg [15:0]seq=16'b1110111011101110;
+  initial begin
+    clk=0;
+    rst=1;
+    x=0;
+    $dumpfile("dump.vcd");
+    $dumpvars(0,tb);
+    $display("clk rst x | y");
+    $monitor("%b %b %b | %b",clk,rst,x,y);
+    #10 rst=0;
+    for(i=0;i<16;i=i+1)begin
+      x=seq[i];
+      #10;
+    end
+    #20$finish;
+  end
+endmodule
