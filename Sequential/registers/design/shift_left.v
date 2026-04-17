c53fffea6da6e5e7b66f3cac914a563a8e8b480f
SaikiranEK <saikiran.e.k.1820@gmail.com>
2026-04-17 19:49:58 +0530

    Create shift_left.v

---
+++ Sequential/registers/design/shift_left.v
@@ -0,0 +1,11 @@
+module left_shift(input clk,
+                  input rst,
+                  input shift,
+                  output reg[3:0] q);
+  always@(posedge clk or posedge rst)begin
+    if(rst)
+      q<=4'b0000;
+    else
+      q<={q[2:0],shift};
+  end
+endmodule
