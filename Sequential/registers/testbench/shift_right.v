0a42ee852fc77ffe64a0a5da18a89ff7b109e7e5
SaikiranEK <saikiran.e.k.1820@gmail.com>
2026-04-17 18:00:19 +0530

    Create shift_right.v

---
+++ Sequential/registers/testbench/shift_right.v
@@ -0,0 +1,31 @@
+module tb;
+
+  reg clk;
+  reg rst;
+  reg shift;
+  wire [3:0]q;
+integer i;
+  right_shift uut(.clk(clk),.rst(rst),.shift(shift), .q(q));
+always #5 clk = ~clk;
+initial begin
+    clk = 0;  
+	rst =1;
+  $dumpfile("dump.vcd");  
+    $dumpvars(0,tb);
+
+  $display("clk rst shift | q");
+  $monitor("%b %b %b | %b",clk,rst,shift, q);
+
+   #10;
+    rst = 0;   
+
+    shift = 1; #10;
+    shift = 0; #10;
+    shift = 1; #10;
+    shift = 1; #10;
+    shift = 0; #10;
+
+    #20 $finish;
+end
+
+endmodule
